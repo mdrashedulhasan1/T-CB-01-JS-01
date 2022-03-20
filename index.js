9aed7723cfd8ec9c1883145e1aeefe4238392ef6
Rashedul Hasan <Rashedul Hasan>
2022-03-21 03:12:46 +0600

    JS-Class-01

---
+++ index.js
@@ -0,0 +1,52 @@
+// var mangoPrice = 10;
+// console.log(mangoPrice);
+
+// var mangoPrice = 20.4235;
+// console.log(typeof mangoPrice);
+
+// var myName = "Tapu";
+// console.log(typeof myName);
+
+// var isGood = true;
+// console.log(typeof isGood);
+// var isBad = false;
+// console.log(typeof isBad);
+
+
+// var myCountry = "I love Bangladesh";
+// var update1 = myCountry.toUpperCase();
+// console.log(update1);
+
+// var update2 = myCountry.toLowerCase();
+// console.log(update2);
+
+// var update3 = myCountry.indexOf('love');
+// console.log(update3);
+
+// var update4 = myCountry.split(' ');
+// console.log(update4);
+
+
+// var num1 = 10;
+// var num2 = parseInt('5');
+// var total = num1+num2;
+// console.log(total);
+
+// var num1 = .2;
+// var num2 = .1;
+// var result = num1+num2;
+// var result = result.toFixed(2);
+// console.log(result);
+
+
+var num1 = 10;
+var num2 = 3;
+var add = num1+num2;
+var sub = num1-num2;
+var multi = num1*num2;
+var div = num1/num2;
+var div = div.toFixed(2);
+var rem = num1%num2;
+console.log(add,sub,multi,div,rem);
+
+
